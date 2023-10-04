969b1436bafd6b04cf74d407c652f67e8c62af8d
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-10-04 19:35:22 +0200

    4 files modified

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Navbar.module.css';
+import {Link} from 'react-scroll'
 
 const Navbar = () => {
   return (
@@ -8,10 +9,10 @@ const Navbar = () => {
         <nav>
           <ul className={styles.ul}>
             <li className={styles.li}>
-              <a hrev="styles.welcomeSection">Home</a>
+              <Link to="/" spy={true} smooth={true} offset={50} duration={500}>Home</Link>
             </li>
             <li className={styles.li}>
-              <a hrev="#">About</a>
+            <Link to="about" spy={true} smooth={true} offset={50} duration={500}>Home</Link>
             </li>
             <li className={styles.li}>
               <a hrev="#">Project</a>
@@ -23,11 +24,11 @@ const Navbar = () => {
         </nav>
       </header>
       <div class={styles.empty}></div>
-      <section id={styles.welcomeSection}>
+      <section id="home" class={styles.welcomeSection}>
         <h1 class={styles.welcomeH1}>Hey I am Dawid</h1>
         <h2 class="welcome-h2"></h2>
       </section>
-      <section id={styles.welcomeSection2}>
+      <section id="About" class={styles.welcomeSection2}>
         <h1 class="welcome-h1">Hey I am Dawid</h1>
         <h2 class="welcome-h2"></h2>
       </section>
