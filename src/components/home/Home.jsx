a896414f94a3c56c2791b9e93af776e3f26b1d83
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-10-10 18:42:13 +0200

    add type writer

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import styles from './Home.module.css';
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
+  const { text } = {
+    words: ['developer', 'bodybulider', 'Enginer'],
+    loop: {},
+  };
   return (
     <div id="home" className={styles.navbarContainer}>
       <section id="home" class={styles.homeSection}>
-        <h1 class={styles.homeH1}>Hey I am Dawid</h1>
-        <h2 class={styles.homeH2}>I am a bodybuilder</h2>
+        <h1 class={styles.homeH1}>Dawid Jaśko</h1>
+        <h2 class={styles.homeH2}>
+          <p className={styles.typeWriterP}>I'm </p>
+          <div className={styles.typeWriter}>
+            <Typewriter
+              options={{
+                strings: [' Enginer', ' bodybulider', ' developer'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
+        </h2>
       </section>
     </div>
   );
