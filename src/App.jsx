ea112def8aadf79eca608e88e7ae1121d62e26f1
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-10-12 02:54:11 +0200

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import Contact from './components/contact/Contact';
 import Home from './components/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Project from './components/project/Project';
-// import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-// import { useState, useEffect, useRef } from "react";
+// import { Parallax } from '@react-spring/parallax'
+// import { useRef } from "react";
 
 function App() {
   // const ref = useRef()
   return (
     <>
+      {/* <Parallax pages={5} ref={ref}> */}
       <Navbar />
       <Home />
       <About />
       <Project />
       <Contact />
+      {/* </Parallax> */}
     </>
   );
 }
