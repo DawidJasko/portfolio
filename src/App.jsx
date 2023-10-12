5924cd1cd2201b9c001100857cc40c8e83a08c0d
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-10-12 02:42:30 +0200

    5 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Contact from './components/contact/Contact';
 import Home from './components/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Project from './components/project/Project';
+// import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+// import { useState, useEffect, useRef } from "react";
 
 function App() {
+  // const ref = useRef()
   return (
     <>
       <Navbar />
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './Home.module.css';
 import Typewriter from 'typewriter-effect';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 const Home = () => {
+  // const ref = useRef();
   return (
     <div id="home" className={styles.navbarContainer}>
+      {/* <Parallax pages={2} ref={ref}> */}
+      {/* <ParallaxLayer offset={0} speed={2.5}> */}
       <div class={styles.empty}></div>
       <section id="home" class={styles.homeSection}>
         <h1 class={styles.homeH1}>Dawid Jaśko</h1>
@@ -26,6 +30,8 @@ const Home = () => {
           </div>
         </h2>
       </section>
+      {/* </ParallaxLayer> */}
+      {/* </Parallax> */}
     </div>
   );
 };
