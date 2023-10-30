9da79b98b571041f31012d1f293403264a5d2fc1
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-10-30 02:23:01 +0100

    add contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,8 +5,33 @@ const Contact = () => {
   return (
     <div id="home" className={styles.navbarContainer}>
       <section id="contact" class={styles.contactSection}>
-        <h1 class={styles.contactH1}>Contact</h1>
-        <h2 class="welcome-h2"></h2>
+      <h1 class={styles.contactH1}>Contact</h1>
+        <div className={styles.conline}>
+          <div className={styles.conclass}>
+          <p>const Dawid Jaśko&#8194; &#61;&#8194;&#40;&#8201;&#41;&#8194;&#61;&#62; &#123;<br/>
+          &#160; useState&#40;&#8201;&#41;&#8194; 
+             &#123; <br/>
+             &#160; this.name = Dawid Jaśko <br/>
+             &#160; this.mail = dawidjasko98@wp.pl
+             &#160; &#160;<br/> &#125;<br/>
+             &#160; workExperiens&#40;&#8201;&#41;&#8194; 
+             &#123; <br/>
+             &#160;  2022 - now: Wielton
+             &#160; <br/>&#125;<br/>
+             &#160;education&#40;&#8201;&#41;&#8194; 
+             &#123; <br/>
+             &#160;2018 - 2022: studia opole
+             &#160;<br/>&#125;<br/>
+             &#160;skills&#40;&#8201;&#41;&#8194; 
+             &#123; <br/>
+             &#160;html itd
+             <br/>&#125;<br/>
+             </p> 
+
+          </div>
+        </div>
+        
+        
       </section>
     </div>
   );
